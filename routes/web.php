4f46bbe8e3c464690ce2d3c7d77c862891e5d055
tunacoOO <furu4188na@gmail.com>
2023-08-01 21:27:37 +0900

    change file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +13,6 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return view('posts.index');
 });
-Route::get('/posts', [PostController::class, 'index']);   
