0ebde31bbc64f9672ec86640405be53bef0f2072
tunacoOO <furu4188na@gmail.com>
2023-08-03 20:29:19 +0900

    retry PostController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;  
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() {
-    return view('posts.index');
-});
+Route::get ('/' ,[Postcontroller::class,'index']);
