8ab95bf46d59e28d43701de4b7e0f839f5a1597f
tunacoOO <furu4188na@gmail.com>
2023-08-06 15:37:18 +0900

    かりきゅらむ08-4/4

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,15 +13,15 @@
             <div class="title">
                 <h2>Title</h2>
                 <input type="text" name="post[title]" placeholder="タイトル">
+                <p class="title_error" style="color:red">{{$errors->first('post.title')}}</p>
             </div>
             <div class="body">
                 <h2>Body</h2>
                 <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。"></textarea>
+                <p class="body_error" style="color:red">{{$errors->first('post.body')}}</p>
             </div>
-            <input type="submit" value="store"/>
+            <input type="submit" value="保存"/>
         </form>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
+        <div class="back">[<a href="/">back</a>]</div>
     </body>
 </html>
