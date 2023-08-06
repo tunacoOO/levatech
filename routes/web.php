c86fce20af05270cf803d6c6bcd87012ebcfa292
tunacoOO <furu4188na@gmail.com>
2023-08-06 18:27:03 +0900

    カリキュラム08-6

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;  
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get ('/' ,[Postcontroller::class,'index']);
+Route::get('/posts/create',[PostController::class,'create']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
+Route::post('/posts',[PostController::class,'store']);
+Route::get('/posts/{post}/edit',[PostController::class,'edit']);
+Route::post('/posts/{post}',[PostController::class,'update']);
+Route::delete('/posts/{post}',[PostController::class,'delete']);
