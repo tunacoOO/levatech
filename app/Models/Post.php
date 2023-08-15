06a6ee6d8d6e4a2eb0ed8ee7fb6f95ae6d1cefc8
tunacoOO <furu4188na@gmail.com>
2023-08-15 22:47:37 +0900

    カリキュラム09-2

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,13 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
-
 {
     use HasFactory;
-    use SoftDeletes;
     
     protected $fillable = [
         'title',
@@ -21,5 +18,10 @@ class Post extends Model
     public function getPaginateByLimit( int $limit_count =  5)
     {
         return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
