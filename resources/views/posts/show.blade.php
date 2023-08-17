bc825354c5f31a0032c0f5d71dc675177ecfe038
tunacoOO <furu4188na@gmail.com>
2023-08-17 20:03:33 +0900

    カリキュラム09-2/5

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,7 +13,7 @@
                <div class='content_post'>
                    <h3>本文</h3>
                     <p class='body'>{{ $post->body }}</p>
-                    <a href="">{{ $post->category->name }}</a>
+                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
                 </div>
         </div>
         <div class="edit">
